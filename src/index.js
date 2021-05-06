3b46ce99aa4eb51089e3b9fe25112d1205dba485
Ethan Rodriguez <ethan.rodriguez@mac.com>
2021-05-05 23:26:49 -0400

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-});
+  const input = document.getElementById('new-task-description');
+  const submitButton = input.nextElementSibling;
+  const ul = document.getElementById('tasks');
+
+submitButton.addEventListener('click', function(e){
+  const li = document.createElement('li'); // creates <li></li>
+  const inputText = input.value;
+  li.innerHTML = inputText
+  ul.appendChild(li);
+  e.preventDefault()
+})
+
+})
+
+
+
+
+
+
+//### Deliverables
+
+//* As a user, I should be able to type a task into the input field.
+///* As a user, I should be able to click some form of a submit button.
+//* As a user, I expect to see the task string that I provided appear in the DOM
+//  after the submit button has been activated
